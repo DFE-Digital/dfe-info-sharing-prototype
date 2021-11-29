b4f625fdd3c234256903bcae86a0a13ebd29fcc6
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2021-11-29 15:47:21 +0000

    Update v1.js

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -7,28 +7,12 @@ const verNum = 1;
 
 
 
-//Version start - start
-
-router.post(`/version-run`, function (req, res) {
-    const referral = req.session.data['referral-service-links'];
-    const directory = req.session.data['directory-service-links'];
-
-    if (referral === 'Version 1') {
-        res.redirect(`/v${verNum}/referrals/index`);
-    } else if (directory === 'Version 1') {
-        res.redirect(`/v${verNum}/directory/index`);
-    }
-});
-
-
-
-// Version start -  end
 
 
 
 
 
 
-
+//*routes to be placed above this line//
 module.exports = router
 
