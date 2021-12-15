6201a5099762a797392d480da4bdddd26b15eec7
BrianMilman <brian.milman@digital.education.gov.uk>
2021-12-15 13:44:33 +0000

    Initial journey - part complete

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -6,7 +6,17 @@ const verNum = 1;
 
 
 
-
+router.post(`/v1/directory/search-type`, function (req, res) {
+  
+    let searchType = req.session.data['child-or-professionals'];
+    
+      if (
+        searchType === 'Child'
+         ) {
+          res.redirect(`/v1/directory/child-search`);
+      }
+     else res.redirect(`/v1/directory/professional-search`);
+    });
 
 
 
