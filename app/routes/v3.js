38caa8ebec07ed6c2db6e3fab1f5da57e929b55f
briananddigital <86655162+BrianMilman@users.noreply.github.com>
2022-01-20 09:50:13 +0000

    Ver 3 - Sprint created

---
+++ app/routes/v3.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const verNum = 2;
+
+
+
+
+router.post(`/v${verNum}/directory/search-type`, function (req, res) {
+  
+    let searchType = req.session.data['child-or-professionals'];
+    
+      if (
+        searchType === 'Child'
+         ) {
+          res.redirect(`/v${verNum}/directory/child-search`);
+      }
+     else res.redirect(`/v${verNum}/directory/professional-search`);
+    });
+
+
+
+
+
+
+//*routes to be placed above this line//
+module.exports = router
+
