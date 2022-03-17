ad4e6f2f67b556411407837e47ec97065593d487
benwatsonuk <hello@benwatson.eu>
2022-03-17 09:09:49 +0000

    V7 pages added and routes updated

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,7 @@ const v3Routes = require('./routes/v3');
 const v4Routes = require('./routes/v4');
 const v5Routes = require('./routes/v5');
 const v6Routes = require('./routes/v6');
+const v7Routes = require('./routes/v7');
 
 
 router.use('/', v1Routes);
@@ -15,5 +16,6 @@ router.use('/', v3Routes);
 router.use('/', v4Routes);
 router.use('/', v5Routes);
 router.use('/', v6Routes);
+router.use('/', v7Routes);
 
 module.exports = router;
