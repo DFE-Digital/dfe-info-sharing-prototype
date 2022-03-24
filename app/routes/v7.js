e364431ee72698d59898b63bd1504b50fb9cc6d3
benwatsonuk <hello@benwatson.eu>
2022-03-24 11:19:48 +0000

    Basic routing for dynamic data

--- app/routes/v7.js
+++ app/routes/v7.js
@@ -3,7 +3,42 @@ const router = express.Router();
 
 const verNum = 7;
 
+// Data Functions
+const createEvents = function () {
+  // Get events from event object
+  // Insert relavent dates (relative to child)
+  // Order chronologically? Split into 'sections' etc.
+  return [
+    {
+      title: 'New attendance',
+      location: 'Netherstowe Primary School',
+      category: 'EDUCATION'
+    }
+  ]
+}
+
+const createProfile = function () {
+  // To dynamically create profile of child
+  return {
+    id: '1',
+    firstName: 'Fred',
+    lastName: 'Bloggs',
+    dob: '10 Feb 2015',
+    dobTimestamp: 1423558877,
+    age: '7',
+    uniqueIdentifier: '746-987-3432',
+    gender: 'Male',
+    ethnicity: 'White British',
+    address: '12 Compton Street, Townsville, Countyville, DR34 5TO'
+  }
+}
 //* Directory routes //
+router.get([`/v${verNum}/directory/record/:childId`, `/v${verNum}/directory/record/:childId/:variantType`], function (req, res) {
+  res.render(`v${verNum}/directory/child-record-dynamic.html`, {
+    events: createEvents(),
+    profile: createProfile()
+  })
+})
 //* Directory routes END //
 
 //* Referral routes //
