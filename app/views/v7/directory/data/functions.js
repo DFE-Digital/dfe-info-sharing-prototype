00301009ba8dd0c696254fddf1652569c850718a
benwatsonuk <hello@benwatson.eu>
2022-03-24 14:27:41 +0000

    Added relative times and sorted by timestamp

--- app/views/v7/directory/data/functions.js
+++ app/views/v7/directory/data/functions.js
@@ -3,7 +3,8 @@ const events = require('./events')
 // Data Functions
 
 const addToDate = function (ogDate, years, months, days) {
-  const date = new Date(ogDate)
+  let date = new Date(ogDate * 1000)
+  console.log(date)
   if (years) {
     days += (years * 365)
   }
@@ -15,11 +16,25 @@ const addToDate = function (ogDate, years, months, days) {
 }
 const createEvents = function (childId, timelineId) {
   // Get events from event object
-  // Insert relavent dates (relative to child)
-  // Order chronologically? Split into 'sections' etc.
   const theEvents = events.events[0].events
-  console.log(theEvents)
-  return theEvents
+  // console.log(theEvents)
+  // Insert relevant dates (relative to child)
+  const theChild = individuals.individuals.filter(x => x.id === childId)[0]
+  // console.log(theChild)
+  const theDob = theChild.dobTimestamp
+  const mappedEvents = theEvents.map(x => {
+    const theEventDate = addToDate(theDob, x.offsetTime.years, x.offsetTime.months, x.offsetTime.days)
+    return {
+      ...x,
+      timestamp: theEventDate,
+      dateString: theEventDate.toLocaleString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })
+    }
+  })
+  // Order chronologically? Split into 'sections' etc.
+  mappedEvents.sort((x, y) => y.timestamp - x.timestamp)
+  console.log(mappedEvents)
+
+  return mappedEvents
   // return [
   //   {
   //     title: 'New attendance',
