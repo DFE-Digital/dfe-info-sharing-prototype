c6c89c691c51678ac7b99e0f1784c78aeac32d7d
benwatsonuk <hello@benwatson.eu>
2022-03-25 12:35:44 +0000

    Chaining journeys together

--- app/routes/v7.js
+++ app/routes/v7.js
@@ -3,6 +3,12 @@ const express = require('express')
 const router = express.Router()
 const data = require(`../views/v${verNum}/directory/data/functions`)
 
+// Create readily available 'locals'
+router.use((req, res, next) => {
+  res.locals.verNum = verNum
+  next()
+})
+
 //* Directory routes //
 router.get([`/v${verNum}/directory/record/:childId`, `/v${verNum}/directory/record/:childId/:designVariant`, `/v${verNum}/directory/record/`], function (req, res) {
   const childId = req.params.childId || '1'
@@ -18,44 +24,44 @@ router.get([`/v${verNum}/directory/record/:childId`, `/v${verNum}/directory/reco
 
 //* Referral routes //
 router.post(`/v${verNum}/referral/immediate-danger`, function (req, res) {
-  let answer = req.session.data['immediatedanger'];
+  let answer = req.session.data['immediatedanger']
 
   if (answer === 'No') {
-    res.redirect(`/v${verNum}/referral/consent`);
+    res.redirect(`/v${verNum}/referral/consent`)
   } else {
-    res.redirect(`/v${verNum}/blocked`);
+    res.redirect(`/v${verNum}/blocked`)
   }
-});
+})
 
 router.post(`/v${verNum}/referral/consent`, function (req, res) {
-  let answer = req.session.data['consent'];
+  let answer = req.session.data['consent']
 
   if (answer === 'Yes') {
-    res.redirect(`/v${verNum}/referral/mash`);
+    res.redirect(`/v${verNum}/referral/mash`)
   } else {
-    res.redirect(`/v${verNum}/blocked`);
+    res.redirect(`/v${verNum}/blocked`)
   }
-});
+})
 
 router.post(`/v${verNum}/referral/mash`, function (req, res) {
-  let answer = req.session.data['mash'];
+  let answer = req.session.data['mash']
 
   if (answer === 'Yes') {
-    res.redirect(`/v${verNum}/referral/mash-assessment`);
+    res.redirect(`/v${verNum}/referral/mash-assessment`)
   } else {
-    res.redirect(`/v${verNum}/referral/ehcp`);
+    res.redirect(`/v${verNum}/referral/ehcp`)
   }
-});
+})
 
 router.post(`/v${verNum}/referral/ehcp`, function (req, res) {
-  let answer = req.session.data['ehcp'];
+  let answer = req.session.data['ehcp']
 
   if (answer === 'Yes') {
-    res.redirect(`/v${verNum}/referral/ehcp-details`);
+    res.redirect(`/v${verNum}/referral/ehcp-details`)
   } else {
-    res.redirect(`/v${verNum}/referral/referral-details-child-educational`);
+    res.redirect(`/v${verNum}/referral/referral-details-child-educational`)
   }
-});
+})
 //* Referral routes END //
 
 //*routes to be placed above this line//
